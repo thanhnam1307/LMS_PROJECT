8fe5e7553d8ea9299a7d2d7571744058fc6835fd
nam nguyen thanh <bffkudo1412@gmail.com>
2023-06-13 23:58:48 +0700

    login

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -1,22 +1,24 @@
-
 from django.contrib import admin
-from django.urls import path,include
-from .import views,user_login
+from django.urls import path, include
+from . import views, user_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('base/',views.BASE,name='base'),
+    path('base/', views.BASE, name='base'),
 
-    path('',views.HOME,name='home'),
+    path('', views.HOME, name='home'),
 
-    path('single/course' ,views.SINGLE_COURSE,name='single_course'),
+    path('single/course', views.SINGLE_COURSE, name='single_course'),
 
-    path('contact/',views.CONTACT_US,name='contact_us'),
+    path('contact/', views.CONTACT_US, name='contact_us'),
 
-    path('about/',views.ABOUT_US,name='about_us'),
+    path('about/', views.ABOUT_US, name='about_us'),
 
-    path('accounts/register', user_login.REGISTER,name='register'),
+    path('accounts/register', user_login.REGISTER, name='register'),
 
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('dologin/' ,user_login.DO_LOGIN,name="dologin"),
+
 ]
--- LMS/LMS/user_login.py
+++ LMS/LMS/user_login.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from app.EmailBackEnd import EmailBackEnd
 
 
 def REGISTER(request):
@@ -29,3 +30,20 @@ def REGISTER(request):
         user.save()
         return redirect('login')
     return render(request, 'registration/register.html')
+
+
+def DO_LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = EmailBackEnd.authenticate(request,
+                                         username=email,
+                                         password=password)
+
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Email and Password Are Invalid !')
+            return redirect('login')
