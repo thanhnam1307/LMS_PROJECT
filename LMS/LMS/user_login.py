99f603548fa53c2351550dc9392c3106fdaad5f3
nam nguyen thanh <bffkudo1412@gmail.com>
2023-06-13 17:10:48 +0700

    login

--- LMS/LMS/user_login.py
+++ LMS/LMS/user_login.py
@@ -1,4 +1,31 @@
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
+from django.contrib import messages
+
 
 def REGISTER(request):
-    return render(request,'registration/register.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(username, email, password)
+
+        # check email
+        if User.objects.filter(email=email).exists():
+            messages.warning(request, 'Email are Already Exists !')
+            return redirect('register')
+
+        # check username
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username are Already exists !')
+            return redirect('register')
+
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+    return render(request, 'registration/register.html')
