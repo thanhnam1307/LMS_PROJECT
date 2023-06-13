ff2090bf6e93c29f59b5c858fb2c744816a9663d
nam nguyen thanh <bffkudo1412@gmail.com>
2023-06-13 16:13:05 +0700

    project_lms

---
+++ LMS/LMS/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from .import views,user_login
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('base/',views.BASE,name='base'),
+
+    path('',views.HOME,name='home'),
+
+    path('single/course' ,views.SINGLE_COURSE,name='single_course'),
+
+    path('contact/',views.CONTACT_US,name='contact_us'),
+
+    path('about/',views.ABOUT_US,name='about_us'),
+
+    path('accounts/register', user_login.REGISTER,name='register'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+]
---
+++ LMS/LMS/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect ,render
+
+def BASE(request):
+    return render(request,'base.html')
+
+def HOME(request):
+    return render(request,'Main/Home.html')
+
+def SINGLE_COURSE(request):
+    return render(request,'Main/single_course.html')
+
+def CONTACT_US(request):
+    return render(request, 'Main/contact_us.html')
+
+def ABOUT_US(request):
+    return render(request, 'Main/about_us.html')
